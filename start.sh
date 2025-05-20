ae69cd551c9d6710c31dfbeed915bed4bc723672
stud1014 <stud1014@sop.ase.ro>
2025-05-20 21:09:17 +0300

    user system start

--- start.sh
+++ start.sh
@@ -6,7 +6,18 @@ show_ui()
 	toilet -f mono9 "Database"
 	printf "$1\n"
 }
+create_user()
+#10
+{
+        printf "$1, $2, $3\n" >> "Users.csv"
+        echo "User adaugat"
+}
 
+search_for_user()
+{
+	grep -F $1 Users.csv 
+}
+#21
 start_screen()
 {
 	read login_option
@@ -14,16 +25,22 @@ start_screen()
 		show_ui "Log In"
 		printf "Email / Username: "
 		read id
-		printf "Password: "
-		read password
-		clear
 
-		#ecriptare parola
-		echo $password > temp.txt #cream un fisier in care stocam parola pentru a utiliza sha256sum
-		encrypted_pass=$(sha256sum temp.txt | sed 's/\s.*//g') #aplicam functia sha256sum si apoi sed pentru a elimina  '__temp.txt' de la final
-		rm temp.txt
+		found=$(search_for_user $id)
+		echo $found
 
-		echo $encrypted_pass
+		if [[ $found -eq 0 ]] ; then
+			printf "Invalid Login Information"
+			sleep 2
+			show_ui "Please select a valid option: log in [0] / sign up [1]"
+			start_screen
+		else
+			
+			printf "Password: "
+			read password
+			clear
+			encrypted_pass=$(echo "$password" | sha256sum | sed 's/\s.*//g')
+		fi
 
 	elif [ $login_option -eq 1 ] ; then
 		show_ui "Sign Up"
@@ -35,11 +52,21 @@ start_screen()
 		read password
 		printf "Reconfirm Password: "
 		read password2
+#49		
 		clear
 
 		if [ $password == $password2 ] ; then
 			#do sign up logic
 			printf "Setting up account...\n"
+			sleep 1
+			printf "While you wait, here is the word of the day"
+
+			encrypted_pass=$(echo "$password" | sha256sum | sed 's/\s.*//g')
+
+			create_user $username $email $encrypted_pass
+			clear
+			printf "Account created!"
+#59
 		else
 			show_ui "Passwords don't match! Press [1] to retry."
 			start_screen
@@ -48,16 +75,6 @@ start_screen()
 		show_ui "Please select a valid option: log in [0] / sign up [1]"
 		start_screen
 	fi
-}
-
-create_user()
-{
-	return
-}
-
-search_for_user()
-{
-	return
 }
 
 show_ui "Welcome! Time to log in [0] or sign up [1]."
